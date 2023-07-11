b303df24d743c41790527b7c0d5682f5cc5d31ef
Red Hat <sonjuhy@gmail.com>
2023-07-11 16:18:22 +0900

    Image test

--- _next/static/IiKfhCyjqPJ3BtpP88NdH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/432d974494c8bb30.css","static/chunks/pages/index-f4a6e8ab622374dc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

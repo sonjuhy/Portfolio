415443d407c8ed06c97e0d0b67286118c56591c6
Red Hat <sonjuhy@gmail.com>
2023-07-11 16:47:37 +0900

    'deploy'

--- out/_next/static/kDprJwHu2lw_07MzKVFzz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/432d974494c8bb30.css","static/chunks/pages/index-5d7dce57660cbd64.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
